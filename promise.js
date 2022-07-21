1e75d6ded299cddf2d93468703d23ce3304cb6fe
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2022-07-21 00:00:32 -0400

    Update promise.js
    Finished promise

--- promise.js
+++ promise.js
@@ -1,10 +1,19 @@
 function WaitForOrders()
 { 
-    return new Promise()
+    return new Promise((resolve, reject) => 
     {
         if($orders === undefined)
         {
-            
+			const maxAttempts = 5;
+            let attemptsLeft = 5;
+			
+			if(attemptLimit === 0)
+				reject(false);
+			
+			let interval = setInterval(() => 
+			{
+				$orders === undefined ? attemptsLeft-- : resolve(true);
+			},1000)
         }
-    }
+    });
 }
