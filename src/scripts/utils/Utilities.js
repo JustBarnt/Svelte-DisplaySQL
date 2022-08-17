d99752ae7bd9f577c6a3bb82e9b583e8900e6860
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2022-08-17 15:42:25 -0400

    Created a utilities class

---
+++ src/scripts/utils/Utilities.js
@@ -0,0 +1,17 @@
+class Utilities
+{
+	/**
+	* Creates a Beautified string.
+	* @param {String} string - an unformatted string to be prettified
+	* @return {String} Returns a formated string
+	*/
+	static BeautifyHeader(string)
+	{
+		let parsedStr = string.replace(/([a-z])([A-Z])/g, "$1 $2");
+		let prettyStr = parsedStr.toLowerCase().split(" ").map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
+		return prettyStr;
+	}
+	
+}
+
+export default Utilities;
