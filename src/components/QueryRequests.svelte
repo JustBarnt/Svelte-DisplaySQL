be22f2ca63a0d1712bdcc2f36c5215447e4c1a82
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2022-08-17 13:41:10 -0400

    Fixed query object, adjusted form

--- src/components/QueryRequests.svelte
+++ src/components/QueryRequests.svelte
@@ -1,59 +1,53 @@
 <script>
 	import { createEventDispatcher } from "svelte";
 
-	const query = { amount: 10, name: "createdby", value: "joshr", startDate: "", endDate: "" };
+	const query = { amount: 10, name: "createdby", value: "joshr", startDate: "2013-07-26", endDate: "2013-07-26" };
 	const queryDispatch = createEventDispatcher();
 
-	let defaultQueryType = "All";
+	let defaultQueryType = "search";
 
 	/**
 	* Starts a specifed query request
 	* @param {String} type - A string specifing the type of query requested
 	*/
-	function QueryRequest(type, params = null)
+	function QueryRequest(type)
 	{
+		const queryString = window.location.search;
+
 		queryDispatch("query",
 		{
 			success: false, 
 			results: null,
 			type: type,
 			message: "Starting up query retrieval.",
-			params: params,
+			params: queryString,
 		});
 	}
 </script>
 
-<!--TODO: Create an HTML conditional menu that swaps between a form(submission query), input(retrieve single query), and button(get all query) -->
-
-
-
-{#if defaultQueryType.toLowerCase() === "all"}
-	<button on:click={() => QueryRequest(defaultQueryType)}>Retrieve all orders</button>
-{:else if defaultQueryType.toLowerCase() === "submit"}
-	<form  method="POST" class="post-form" on:submit={() => QueryRequest(defaultQueryType, query)}>
+<div id="container">
+	<form  method="GET" class="get-form" on:submit|preventDefault={() => QueryRequest(defaultQueryType)}>
 		<span>
 			Number of Results<br>
-			<input type="number" id="amount" name="amount" bind:value={query.amount}>
+			<input type="number" id="amount" name="Amount" bind:value={query.amount}>
 		</span>
 		<br>
 		<span>
 			Tag to filter by<br>
-			<input type="text" id="name" name="name" bind:value={query.name}>
+			<input type="text" id="name" name="Name" bind:value={query.name}>
 		</span>
 		<br>
 		<span>
 			Value of the tag<br>
-			<input type="text" id="value" name="value" bind:value={query.value}>
+			<input type="text" id="value" name="Value" bind:value={query.value}>
 		</span>
 		<br>
 		<span>
 			Created between<br>
-			<input type="date" id="startDate" name="start date" bind:value={query.startDate}>
-			<input type="date" id="endDate" name="end date" bind:value={query.endDate}>
+			<input type="date" id="startDate" name="StartDate" bind:value={query.startDate}>
+			<input type="date" id="endDate" name="EndDate" bind:value={query.endDate}>
 		</span>
 		<br>
 		<button type='submit'>Search for Licenses</button>
 	</form>
-{:else}
-	<p>Single query searching isn't inplemented</p>
-{/if}
+</div>
