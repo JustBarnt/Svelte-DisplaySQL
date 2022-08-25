739dcf05df146e3695c3793d358d5a8128af9b6a
Brent Williams <85238383+JustBarnt@users.noreply.github.com>
2022-08-25 16:46:23 -0400

    Daily update

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,26 +1,27 @@
 <script>
-		const formValues = { amount: "10", column: "createdby", value: "joshr", startDate:"", endDate:"" };
+	const formValues = { amount: "", column: "", value: "", startDate:"", endDate:"" };
 </script>
 
 <div id="container">
-	<form method="GET" class="get-form" action="/Licenses">
+	<form method="GET" action="/Licenses">
 		
 		<label for="amount">Top Results:</label>
-		<input type="text" id="amount" name="Amount" placeholder="Number of responses" value="">
+		<input type="text" id="amount" name="Amount" placeholder="Number of results" bind:value={formValues.amount}>
 	
 		<label for="name">With Tag:</label>
-		<input type="text" id="name" name="Column" placeholder="Tag name" value="">
+		<input type="text" id="name" name="Column" placeholder="Tag to search" bind:value={formValues.column}>
 	
 		<label for="value">Tag Value:</label>
-		<input type="text" id="value" name="Value" placeholder="Tag value" value="">
+		<input type="text" id="value" name="Value" placeholder="Tag value to search by" bind:value={formValues.value}>
 	
 		<label for="startDate">Earliest Date:</label>
-		<input type="text" id="startDate" name="StartDate" placeholder="Earliest Date" value="">
+		<input type="text" id="startDate" name="StartDate" placeholder="Earliest date available" bind:value={formValues.startDate}>
 	
 		<label for="endDate">Latest Date:</label>
-		<input type="text" id="endDate" name="EndDate" placeholder="Latest Date" value="">
+		<input type="text" id="endDate" name="EndDate" placeholder="Latest date available" bind:value={formValues.endDate}>
 
-		<input type="submit" value="Search"/>
+		<input type="submit" value="Search" />
+		
 	</form>
 </div>
 
@@ -31,7 +32,7 @@
 
 	form{
 		display: grid;
-		width:80%;
+		width:35%;
 		border: 0.25rem solid #24242450;
 		border-radius: 1rem;
 		background-color: var(--headerBG);
@@ -40,15 +41,6 @@
 		transition: border-color 0.25s, box-shadow 0.25s;
 	}
 
-	form:hover{
-		border-color: var(--buttonBorder);
-		box-shadow: 0 0 0.5rem 0.5rem #646cff50;
-	}
-
-	form:focus{
-		outline: 0.4rem auto -webkit-focus-ring-color;
-	}
-	
 	input{
 		padding: 1rem 2rem;
 		margin: 1.5rem 0;
